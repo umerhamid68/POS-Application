861dd3a469e528dd492c93e6bfcb07f1da57fec4
umerhamid68 <umerhamid168@gmail.com>
2025-01-20 20:11:13 +0500

    updates - antd tokens, eslint, build scripts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+// import "@/styles/globals.css";
+// import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />;
+// }
+
+import React from 'react';
+import { ConfigProvider } from 'antd';
+import type { AppProps } from 'next/app';
+
+import theme from 'themes/themeConfig';
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <ConfigProvider theme={theme}>
+    <Component {...pageProps} />
+  </ConfigProvider>
+);
+
+export default App;
---
+++ src/themes/themeConfig.ts
@@ -0,0 +1,10 @@
+import type { ThemeConfig } from 'antd';
+
+const theme: ThemeConfig = {
+  token: {
+    fontSize: 21,
+    colorPrimary: '#52c41a',
+  },
+};
+
+export default theme;
